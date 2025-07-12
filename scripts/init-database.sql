a58fc2f1e796a18a21e72c1209ef4baeb7fd3d63
gerson <gerson.segura.perez@gmail.com>
2025-07-12 05:21:01 -0500

    test 4

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 CREATE TABLE IF NOT EXISTS clientes (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     telefono VARCHAR(20) NULL,
     direccion VARCHAR(255) NULL,
     activo BOOLEAN DEFAULT true,
